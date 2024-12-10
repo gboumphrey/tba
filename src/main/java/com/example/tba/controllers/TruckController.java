18443b6e1a37f7f497a0dd822da4ed921dcef2a1
gboumphrey <74275873+gboumphrey@users.noreply.github.com>
2024-12-10 12:30:32 +0000

    some logging

--- src/main/java/com/example/tba/controllers/TruckController.java
+++ src/main/java/com/example/tba/controllers/TruckController.java
@@ -5,6 +5,8 @@ import com.example.tba.input.TruckCreateInput;
 import com.example.tba.input.TruckUpdateInput;
 import com.example.tba.repositories.TruckHaulierRepository;
 import com.example.tba.repositories.TruckRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 public class TruckController {
+    private static final Logger logger = LoggerFactory.getLogger(TruckController.class);
     @Autowired
     TruckRepository truckRepository;
     @Autowired
@@ -38,6 +41,9 @@ public class TruckController {
         truck.setTare(input.getTare());
         truck.setNett(input.getNett());
         truck.setHaulier(input.getHaulier());
+        if (input.getGross() == 0 || input.getNett() == 0 || input.getTare() == 0) {
+            logger.warn("Weight values 0 - perhaps not entered");
+        }
         return truckRepository.save(truck);
     }
 
@@ -49,12 +55,16 @@ public class TruckController {
         truck.setTare(input.getTare());
         truck.setNett(input.getNett());
         truck.setHaulier(input.getHaulier());
+        if (input.getGross() == 0 || input.getNett() == 0 || input.getTare() == 0) {
+            logger.warn("Weight values 0 - perhaps not entered");
+        }
         return truckRepository.save(truck);
     }
 
     @DeleteMapping("/trucks/{id}")
     public String deleteTruck(@PathVariable Short id) {
         Truck truck = truckRepository.findById(id).orElseThrow(()->new ResponseStatusException(NOT_FOUND, "No truck with id"));
+        logger.info("Deleting truck id {}", id);
         truckRepository.deleteById(id);
         return "Truck was deleted";
     }
